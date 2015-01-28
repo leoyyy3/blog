75f2d44fca127a0a92994485bf50bacbfc38e359
420303844@qq.com <admin_123>
2015-01-28 18:04:13 +0800

    ...

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 var crypto = require('crypto');
-var User = require('../models/user.js')
+var User = require('../models/user.js');
+var Post = require('../models/post.js');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {//console.log(req.session.user.name)
-  res.render('index', { 
-  	title: '主页' ,
-  	user: req.session.user,
-  	success: req.flash('success').toString(),
-  	error: req.flash('error').toString()
-  });
+  Post.get(null,function(err,posts){
+    if(err){
+      posts = [];
+    }
+    res.render('index', { 
+      title: '发表' ,
+      user: req.session.user,
+      posts:posts,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    })
+  })
 });
 
 router.get('/hello',function(req,res){
@@ -96,12 +103,31 @@ router.post('/login', function(req, res) {
 
 router.get('/post',checkLogin);
 router.get('/post', function(req, res) {
-  res.render('post', { title: '发表' });
+  
+    res.render('post', { 
+      title: '发表' ,
+      user: req.session.user,
+      
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    })
+ 
+  
 });
 
 router.post('/post',checkLogin);
 router.post('/post', function(req, res) {
-  res.render('post', { title: '发表' });
+  var currentUser = req.session.user,
+  post = new Post(currentUser.name,req.body.title,req.body.post);
+  post.save(function(err){
+    if(err){
+      req.flash('error',err);
+      return res.redirect('/');
+    }
+    req.flash('success','发布成功');
+    res.redirect('/');
+  })
+
 });
 
 router.get('/logout',checkLogin);
